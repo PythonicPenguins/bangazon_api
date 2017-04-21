1d63c115d47c34c54f464f1d1eb6e7d4aa07ea91
Meg Ducharme <ducharme.meg@gmail.com>
2017-04-21 14:56:20 -0500

    Updated Customer model

--- bangazon/bangazon_api/models.py
+++ bangazon/bangazon_api/models.py
@@ -5,3 +5,4 @@ class Customer(models.Model):
     first_name = models.CharField(max_length=55)
     last_name = models.CharField(max_length=55)
     date_account_created = models.CharField(max_length=22)
+    status = model.BooleanField(initial=True)
