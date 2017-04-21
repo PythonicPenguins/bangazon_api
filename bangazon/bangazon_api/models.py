48678508991afea743bf9230e6c7f8a6c64aaf97
Meg Ducharme <ducharme.meg@gmail.com>
2017-04-21 14:30:42 -0500

    Updated file names, imports and urls

--- bangazon/bangazon_api/models.py
+++ bangazon/bangazon_api/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    first_name = models.CharField(max_length=55)
+    last_name = models.CharField(max_length=55)
+    date_account_created = models.CharField(max_length=22)
--- bangazon/bangazon_api/views.py
+++ bangazon/bangazon_api/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from bangazon_api.models import *
+from bangazon_api.serializers import *
 
-# Create your views here.
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class CustomerViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
